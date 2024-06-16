e1799ac0a76fe7f3507d3ed4eeb34b46f720b95c
RamiKrispin <ramkrisp@umich.edu>
2024-06-16 08:11:07 -0700

    remove template files

--- .devcontainer/install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Install dependecies
- apt-get update && apt-get install -y --no-install-recommends \
-    python3-launchpadlib \
-    vim \
-    && rm -rf /var/lib/apt/lists/*
-
